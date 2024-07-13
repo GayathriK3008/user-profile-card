f576cb90cf51adba1cf27c8022d96cca4971cf08
GayathriK3008 <gayathri3082004@gmail.com>
2024-07-14 01:34:10 +0530

    Initial commit - Added User Profile Card project files

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for User Profile Card interactivity
+
+// Function to display an alert when the Contact button is clicked
+document.getElementById("contact-button").addEventListener("click", function() {
+    alert("Contact button clicked!");
+});

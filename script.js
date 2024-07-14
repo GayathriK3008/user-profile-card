9e86858f5ed71d71851aabe4f6ac11457e912683
GayathriK3008 <gayathri3082004@gmail.com>
2024-07-15 00:12:00 +0530

    Updated styles for larger elements and adjusted LinkedIn icon size

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 
 // Function to display an alert when the Contact button is clicked
 document.getElementById("contact-button").addEventListener("click", function() {
-    alert("Contact button clicked!");
+    alert("Please contact Gayathri at gayathri3082004@gmail.com for further inquiries.");
 });
